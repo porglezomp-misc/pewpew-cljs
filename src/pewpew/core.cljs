1a689b94f3c14e3600d9e3106db190ac255ff6d5
Caleb Jones <code@calebjones.net>
2016-08-11 15:33:25 -0400

    Add support for extracting tile colliders

--- src/pewpew/core.cljs
+++ src/pewpew/core.cljs
@@ -12,7 +12,8 @@
 (def container (r/adapt-react-class js/ReactPIXI.DisplayObjectContainer))
 
 (defn indexed [s] (map-indexed vector s))
-(defn log [x] (.log js/console x))
+(defn log [& x] (apply (.-log js/console) x))
+(defn dbg [x] (doto x log))
 
 (set! js/PIXI.SCALE_MODES.DEFAULT js/PIXI.SCALE_MODES.NEAREST)
 
@@ -30,19 +31,62 @@
 
 (def tile-textures
   (reaction
-    (if-let [tilesets (get-in @game-world [:tilemap :tilesets])]
-      (apply merge
-        (for [tileset tilesets
-              tileid (range (:tilecount tileset))]
-          (let [id (+ (:firstgid tileset) tileid)
-                [w h] (:tilesize tileset)
-                [x y] ((juxt mod quot) tileid (:rows tileset))]
-            {id (make-tile-texture tilemap-texture x y w h)}))))))
+   (if-let [tilesets (get-in @game-world [:tilemap :tilesets])]
+     (apply merge
+            (for [tileset tilesets
+                  tileid (range (:tilecount tileset))]
+              (let [id (+ (:firstgid tileset) tileid)
+                    [w h] (:tilesize tileset)
+                    [x y] ((juxt mod quot) tileid (:rows tileset))]
+                {id (make-tile-texture tilemap-texture x y w h)}))))))
 
 (def spawn-points
   (reaction
-    (if-let [objects (get-in @game-world [:tilemap :objects])]
-      (filter #(-> % :type (= "Spawn")) objects))))
+   (if-let [objects (get-in @game-world [:tilemap :objects])]
+     (filter #(-> % :type (= "Spawn")) objects))))
+
+(def player-texture (.fromImage js/PIXI.Texture "/img/player0.png"))
+
+(def player-frames
+  (vec (for [i (range 4)]
+         (js/PIXI.Texture. player-texture (js/PIXI.Rectangle. (* 16 i) 0 16 16)))))
+
+(defn merge-matrices
+  "Takes a sequence of matrices (a (vec (vec item))) and returns a
+  matrix containing vectors of the respective elements of each matrix,
+  dropping any nil elements or nil matrices in the process."
+  [matrices]
+  (when-not (empty? matrices)
+    (let [filter-falsy (partial filter identity)
+          non-nil-vec (comp vec filter-falsy vector)
+          merge-row (partial (comp vec map) non-nil-vec)
+          merge-matrix (partial (comp vec map) merge-row)]
+      (->> matrices
+           filter-falsy
+           (apply merge-matrix)))))
+
+(def tile-width (r/cursor game-world [:tilemap :tile-width]))
+(def tile-height (r/cursor game-world [:tilemap :tile-height]))
+
+(defn make-collision-grid
+  []
+  (let [layers (get-in @game-world [:tilemap :layers])
+        tile-width @tile-width
+        tile-height @tile-height]
+    (merge-matrices
+     (for [layer layers]
+       (when (get-in layer [:properties :solid] false)
+         (for [[rowid row] (indexed (:data layer))]
+           (for [[colid tile] (indexed row)]
+             (when (pos? tile)
+               (let [x0 (* colid tile-width)
+                     y0 (* rowid tile-width)
+                     x1 (+ x0 tile-width)
+                     y1 (+ y0 tile-height)]
+                 {:tile tile
+                  :bbox [x0 y0 x1 y1]})))))))))
+
+(def collision-grid (r/track! make-collision-grid))
 
 (defn tile
   [{:keys [x y texture]}]
@@ -51,30 +95,37 @@
 
 (defn tile-row
   [i row]
-  [container {:x 0 :y (* i 16)}
+  [container {:y (* i 16)}
    (for [[i tile-id] (indexed row)]
      (when (pos? tile-id)
        ^{:key i} [tile {:x (* i 16) :y 0 :texture tile-id}]))])
 
 (defn layer
   [the-layer]
-  [container {:x 0 :y 0}
+  [container {}
    (for [[i row] (indexed (:data the-layer))]
      ^{:key i} [tile-row i row])])
 
 (defn tilemap
   [the-map]
-  [container {:x 7 :y 90 :scale (js/PIXI.Point. 1 1)}
+  [container {}
    (for [the-layer (:layers the-map)]
      ^{:key (:name the-layer)} [layer the-layer])])
+
+(defn player
+  [{:keys [x y]}]
+  [sprite {:x x :y y :texture (player-frames 0)}])
 
 (defn game-root
   [world]
   [stage {:width 400 :height 300
           :backgroundcolor (some-> (get-in @world [:tilemap :background-color])
-                             (clojure.string/replace #"#" "0x")
-                             js/Number)}
-   [tilemap (:tilemap @world)]])
+                                   (clojure.string/replace #"#" "0x")
+                                   js/Number)}
+   [container {:x 7 :y 90 :scale (js/PIXI.Point. 1 1)}
+    [tilemap (:tilemap @world)]
+    (for [[i the-player] (indexed (:players @world))]
+      ^{:key i} [player the-player])]])
 
 (r/render-component [game-root game-world]
                     (. js/document (getElementById "app")))
--- src/tmx/core.cljs
+++ src/tmx/core.cljs
@@ -4,7 +4,8 @@
             [cljs-http.client :as http]
             [cljs.core.async :refer [<! >!]]))
 
-(defn log [x] (.log js/console x))
+(defn log [& x] (apply (.-log js/console) x))
+(defn dbg [x] (doto x log))
 
 (defn ->tileset
   [obj]
@@ -22,6 +23,25 @@
      :rows (.ceil js/Math (/ tilecount columns))
      :tilecount (js/Number tilecount)}))
 
+(defmulti ->value (comp keyword :type) :default nil)
+(defmethod ->value nil [{:keys [value]}] value)
+(defmethod ->value :string [{:keys [value]}] value)
+(defmethod ->value :int [{:keys [value]}] (js/Number value))
+(defmethod ->value :float [{:keys [value]}] (js/Number value))
+(defmethod ->value :bool
+  [{:keys [value]}]
+  (if (nil? value)
+    false
+    (= value "true")))
+
+(defn ->properties
+  [prop-blocks]
+  (into {}
+    (for [block prop-blocks
+          prop (:content block)]
+      [(-> prop :attributes :name keyword)
+       (->value (:attributes prop))])))
+
 (defn ->layer
   [obj]
   (let [{:keys [width height name]} (:attributes obj)
@@ -32,11 +52,15 @@
                (partition (js/Number width))
                (take (js/Number height))
                (map vec)
-               vec)]
+               vec)
+        properties (->> (:content obj)
+                     (filter #(-> % :tag (= :properties)))
+                     ->properties)]
     {:name name
      :width (js/Number width)
      :height (js/Number height)
-     :data data}))
+     :data data
+     :properties properties}))
 
 (defn ->object
   [obj]
@@ -72,10 +96,10 @@
                   (filter #(-> % :tag (= :objectgroup)))
                   (mapcat ->objectgroup)
                   vec)]
-    {:width width
-     :height height
-     :tile-width tilewidth
-     :tile-height tileheight
+    {:width (js/Number width)
+     :height (js/Number height)
+     :tile-width (js/Number tilewidth)
+     :tile-height (js/Number tileheight)
      :background-color backgroundcolor
      :tilesets tilesets
      :layers layers
